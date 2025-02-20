278bc5009e37c05488d0fd494321ee202dfedb8d
Ayy-Ayy Ron <pasadill211@gmail.com>
2025-02-21 01:01:18 +0800

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import '../App'; 
+import '../App.css';
 
 
 const Hero = () => {
